45ddfa5f73c8ca3767ef3e27d7fe15e371361e4d
matheustkaczyk <matheus111904@hotmail.com>
2022-02-17 15:27:11 -0300

    Tela do recebidor, ja recebendo informaçoes sobre cnpj e dados sobre comprador

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 import json
 
 # Importando arquivos de dicionário que contém XPATHS
-from path_index import login_index, main_index, emition_index
+from path_index import login_index, main_index, emition_index, receiver_index
 
 load_dotenv('.env')
 
@@ -30,59 +30,82 @@ driver = webdriver.Firefox()
 driver.get(URL)
 
 with open('clients.json') as clients:
-  jsonClients = list(json.load(clients).items())
-  clients.close()
+  clients_json = list(json.load(clients).items())
 
-# Página de login elementos
-  time.sleep(.5)
+  target_cnpj = '75578914000214'
 
-  input_login = driver.find_element(By.XPATH, login_index['login_input'])
-  input_password_login = driver.find_element(By.XPATH, login_index['login_password'])
-  login_btn = driver.find_element(By.XPATH, login_index['login_btn'])
+  for client in clients_json:
+    if client[1]['CNPJ'] == target_cnpj:
+  # Página de login elementos
+      time.sleep(.5)
 
-  input_login.send_keys(CPF)
-  input_password_login.send_keys(SENHA)
-  login_btn.click()
+      input_login = driver.find_element(By.XPATH, login_index['login_input'])
+      input_password_login = driver.find_element(By.XPATH, login_index['login_password'])
+      login_btn = driver.find_element(By.XPATH, login_index['login_btn'])
 
-# Tela principal
+      input_login.send_keys(CPF)
+      input_password_login.send_keys(SENHA)
+      login_btn.click()
 
-  time.sleep(.5)
+    # Tela principal
 
-  main_selector_wrapper = driver.find_element(By.XPATH, main_index['main_selector_wrapper'])
-  main_selector_wrapper.click()
+      time.sleep(.5)
 
-  time.sleep(1)
+      main_selector_wrapper = driver.find_element(By.XPATH, main_index['main_selector_wrapper'])
+      main_selector_wrapper.click()
 
-  main_selector = driver.find_element(By.XPATH, main_index['main_selector'])
-  main_selector.click()
+      time.sleep(1)
 
-# Tela de emissão
+      main_selector = driver.find_element(By.XPATH, main_index['main_selector'])
+      main_selector.click()
 
-  time.sleep(.5)
+    # Tela de emissão
 
-  # Scroll para o fim da página para encontrar todos os itens
-  driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+      time.sleep(.5)
 
-  time.sleep(.5)
+      # Scroll para o fim da página para encontrar todos os itens
+      driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
-  street_info_me = driver.find_element(By.XPATH, emition_index['street_info'])
-  street_number_me = driver.find_element(By.XPATH, emition_index['street_number'])
-  neighborhood_name_me = driver.find_element(By.XPATH, emition_index['neighborhood_name'])
-  emition_btn = driver.find_element(By.XPATH, emition_index['emition_btn'])
+      time.sleep(.5)
 
-  #Garantindo que os inputs estão vazios
-  street_info_me.send_keys(Keys.CONTROL + 'a')
-  street_info_me.send_keys(Keys.DELETE)
-  street_number_me.send_keys(Keys.CONTROL + 'a')
-  street_number_me.send_keys(Keys.DELETE)
-  neighborhood_name_me.send_keys(Keys.CONTROL + 'a')
-  neighborhood_name_me.send_keys(Keys.DELETE)
-  #######################################
+      street_info_me = driver.find_element(By.XPATH, emition_index['street_info'])
+      street_number_me = driver.find_element(By.XPATH, emition_index['street_number'])
+      neighborhood_name_me = driver.find_element(By.XPATH, emition_index['neighborhood_name'])
+      emition_btn = driver.find_element(By.XPATH, emition_index['emition_btn'])
 
-  street_info_me.send_keys(RUA)
-  street_number_me.send_keys(NUMERO)
-  neighborhood_name_me.send_keys(BAIRRO)
+      #Garantindo que os inputs estão vazios
+      street_info_me.send_keys(Keys.CONTROL + 'a')
+      street_info_me.send_keys(Keys.DELETE)
+      street_number_me.send_keys(Keys.CONTROL + 'a')
+      street_number_me.send_keys(Keys.DELETE)
+      neighborhood_name_me.send_keys(Keys.CONTROL + 'a')
+      neighborhood_name_me.send_keys(Keys.DELETE)
+      #######################################
 
-  time.sleep(.5)
+      street_info_me.send_keys(RUA)
+      street_number_me.send_keys(NUMERO)
+      neighborhood_name_me.send_keys(BAIRRO)
 
-  emition_btn.click()
+      time.sleep(.5)
+
+      emition_btn.click()
+
+      time.sleep(.5)
+
+      # Tela identificação do recebidor
+
+      # Identificação parte 1
+      receiver_radio = driver.find_element(By.XPATH, receiver_index['receiver_type_btn'])
+      receiver_cnpj_input = driver.find_element(By.XPATH, receiver_index['receiver_input_cnpj'])
+      receiver_ie_input = driver.find_element(By.XPATH, receiver_index['receiver_ie'])
+      receiver_fantasy_name = driver.find_element(By.XPATH, receiver_index['receiver_fantasy_name'])
+
+      receiver_radio.click()
+      time.sleep(1)
+
+      receiver_cnpj_input.click()
+      receiver_cnpj_input.send_keys(Keys.HOME + client[1]['CNPJ'])
+      receiver_ie_input.send_keys(Keys.HOME + client[1]['IE'])
+      receiver_fantasy_name.send_keys(Keys.HOME + client[1]['NOME_FANTASIA'])
+
+      # Identificação parte 2
